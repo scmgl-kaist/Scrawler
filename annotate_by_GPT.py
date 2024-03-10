042419338927a2fe0088cbe47c84bfd8c6e4e186
cheongsungmin <cheongsungmin@gmail.com>
2024-03-11 02:36:27 +0900

    Create annotate_by_GPT.py

---
+++ annotate_by_GPT.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+def read_input(input_filename):
+    print("")
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print("Usage: python annotate_by_GPT.py [input_file] [output_file]")
+        exit()
+
+    # Read and process input
+    df, series_list = read_input(sys.argv[1])
+
+    # Generate output file
+    df.to_csv(sys.argv[2], index=False)
+    print(f"\nYou can check your result in: {sys.argv[2]}\n")
+
+
